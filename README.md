cc7254c819e38ee94c1aef757c18b9ea3fb5be6e
LEETAEHO <tae1560@gmail.com>
2012-11-30 12:37:23 +0900

    Initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-2012_db_project
-===============
-
-2012_db_project on yonsei univ.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbProject::Application.config.secret_token = 'a83c0cd4354c3c3c8840ca17d7c5efde053dc58949391534611fef2dd47ea5fcf090ee6f2fd7d00d209d35943ded0f8aadd46dccfde46c5df890ad44961da1cd'
